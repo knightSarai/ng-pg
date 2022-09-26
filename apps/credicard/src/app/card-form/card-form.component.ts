f4f6b9613b3b71d4a415b8202e6843a37e4dfcb5
knightSarai <knight.sarai.dev@gmail.com>
2022-09-26 23:20:57 +0300

    Finish form structure

--- apps/credicard/src/app/card-form/card-form.component.ts
+++ apps/credicard/src/app/card-form/card-form.component.ts
@@ -13,10 +13,37 @@ export class CardFormComponent implements OnInit {
       nonNullable:true,
       validators: [Validators.required, Validators.minLength(3)] 
     }),
+    number: new FormControl('', {
+      nonNullable:true,
+      validators: [
+        Validators.required,
+        Validators.minLength(16),
+        Validators.maxLength(16),
+      ] 
+    }),
+    expiration: new FormControl('', {
+      nonNullable:true,
+      validators: [
+        Validators.required,
+        Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/),
+      ] 
+    }),
+    securityCode: new FormControl('', {
+      nonNullable:true,
+      validators: [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(3),
+      ] 
+    }),
   })
   
 
   ngOnInit(): void {
     this.cardForm.controls
+  }
+
+  onSubmit() {
+    console.log(this.cardForm.value);
   }
 }
