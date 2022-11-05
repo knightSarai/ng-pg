1833f045c49e0bf07a91e9f82f07d13ecbab0cb5
knightSarai <knight.sarai.dev@gmail.com>
2022-11-05 21:48:48 +0300

    Change nav bar to use bootstrap

--- apps/ng-pg/src/app/app.module.ts
+++ apps/ng-pg/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
